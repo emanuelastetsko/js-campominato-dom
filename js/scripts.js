1d5de0d43955f5a903d9a5da1d87d52c6ff76cef
Emanuela Stetsko <emanuelastetsko.es@gmail.com>
2023-01-18 13:18:11 +0100

    Array bombe e gioco

--- js/scripts.js
+++ js/scripts.js
@@ -1,44 +1,85 @@
 const container = document.getElementById("container");
+const grid = document.getElementById("grid");
 
+// All'apertura della pagina non si visualizza nesssuna tabella 
 container.style.display = "none";
 
 
-const grid = document.getElementById("grid");
+// Funzione per generare la tabella
+function generaTabella() {
+ 
 
-// Ciclo per generazione delle celle 
-for (let index = 1; index <= 100; index++) {
+    let bombe= [];
+    let partitaInCorso= true;
+ 
     
-    // Creazione cella 
-    const cell = document.createElement("div");
-    cell.classList.add("square");
-    grid.append(cell);
-
-    //Al click colorare le celle di rosa 
-    cell.addEventListener("click",
-        function () {
-            console.log("Hai cliccato la cella n." + index);
-            
-            if(cell.classList.contains("click")){
-                cell.classList.remove("click");
-            }
-            else{
-                cell.classList.add("click");
-            }
-
+    while(bombe.length < 16){
+        let numeroRandom = Math.floor(Math.random() * 100) + 1;
+        if(!bombe.includes(numeroRandom)){
+            bombe.push(numeroRandom);
         }
-    );
+    }
 
     
+    // Ciclo per generazione delle celle
+    for (let index = 1; index <= 100; index++) {
+        
+        // Creazione cella 
+        const cell = document.createElement("div");
+        cell.classList.add("square");
+        grid.append(cell);
+
+        // Visualizzazione in pagina del numero delle celle 
+        let cellNumber = document.createElement("div");
+        cellNumber = [index];
+        cell.append(cellNumber);
+        
+
+        //Al click controllare se c'è una bomba o meno e colorare le celle del colore appropriato  
+        cell.addEventListener("click",
+            function () {
+
+                console.log("Hai cliccato la cella n." + index);
+                
+                if((!cell.classList.contains("red") || !cell.classList.contains("red")) && partitaInCorso){
+                    if(bombe.includes(index)){
+
+                        console.log("Hai perso");
+                        partitaInCorso=false;
+                        cell.classList.add("red");
+                        
+                    }
+                    else{
+                       
+                        cell.classList.add("blue");
+
+                    }
+                }
+            }
+        );       
+    }
 }
+
+
+// Funzione per svuotare la griglia
+function svuotaTabella() {
+    // Svuoto la tabella
+    grid.innerHTML = "";
+    console.log("La tabella è stata svuotata");
+}
+
 
 
 const myButton = document.getElementById("play");
 
-// Al click deve apparire la griglia con tutte le sue celle
+// Al click deve apparire la griglia (precedentemente nascosta) con tutte le sue celle
 myButton.addEventListener("click",
 
   function () {
-
+    svuotaTabella();
+    generaTabella();
+    
+      
     container.style.display = "inline";
     console.log("Il gioco è iniziato");
     
